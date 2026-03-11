3bc004bf0b808971addedaf816b9dfae7eb23ee4
Padhoab <aniruddh.seth.123@gmail.com>
2026-03-11 08:18:05 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,26 @@
+let quiz = [
+
+{
+question: "Who is PM of India?",
+options: ["Narendra Modi","Rahul Gandhi","Amit Shah","Yogi Adityanath"],
+correct: 0
+},
+
+{
+question: "Capital of India?",
+options: ["Delhi","Mumbai","Kolkata","Chennai"],
+correct: 0
+},
+
+{
+question: "UPSC stands for?",
+options: [
+"Union Public Service Commission",
+"United Public Service Commission",
+"Union Police Service Commission",
+"Universal PSC"
+],
+correct: 0
+}
+
+];
