cb834040f09687387bc4cf7c5d00cae48361212a
Padhoab <aniruddh.seth.123@gmail.com>
2026-06-20 21:40:34 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,64 @@
+import { auth, db } from "./firebase.js";
+
+import {
+createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+doc,
+setDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const signupBtn = document.getElementById("signupBtn");
+
+if (signupBtn) {
+
+signupBtn.addEventListener("click", async () => {
+
+const name = document.getElementById("name").value;
+
+const email = document.getElementById("email").value;
+
+const password = document.getElementById("password").value;
+
+try {
+
+const userCredential = await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+const user = userCredential.user;
+
+await setDoc(doc(db, "users", user.uid), {
+
+name: name,
+
+email: email,
+
+createdAt: new Date(),
+
+testsAttempted: 0,
+
+totalScore: 0,
+
+accuracy: 0
+
+});
+
+alert("Account created successfully!");
+
+window.location.href = "login.html";
+
+}
+
+catch (error) {
+
+alert(error.message);
+
+}
+
+});
+
+}
